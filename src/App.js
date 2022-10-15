095d4390b6f8f274a63d01b444bc1107c59e5ddf
Atun <mali1996ua@gmail.com>
2022-10-15 12:05:28 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-// import logo from "./logo.svg";
-// import "./App.css";
+//! React Bootstrap sevilen CSS frameworkü olan Bootstrapın React için uyarlanmış halidir. Bootstrap temelli çalışır ve fakat komponetler birer React JS componenti olarak tasarlanmıştır. Diğer bir deyimle Bootstrapın React yorumudur.
+
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
+
+//! ortak veri kullanilacak yerde bir ust parent'ta kullanilmali ve lazim
+//! olacak yerlerde prop yoluyla gondermeliyiz.
+//! Bu islemin adi ise Lifting-up State
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Container className="text-center mt-4">
+      <Header />
+      <Card />
+    </Container>
   );
 }
 
